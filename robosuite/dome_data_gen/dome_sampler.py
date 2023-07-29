9da4f051b89acd1e7677356a4625e8a8605e1d95
Aaditya Prasad <aadityap@stanford.edu>
2023-07-28 23:35:17 -0700

    makes demo file now

--- robosuite/dome_data_gen/dome_sampler.py
+++ robosuite/dome_data_gen/dome_sampler.py
@@ -15,13 +15,17 @@ import robosuite as suite
 from robosuite.wrappers import DataCollectionWrapper
 from robosuite import load_controller_config
 
+import datetime
+import h5py
+
 
 
 def collect_sampled_trajectory(env, timesteps=1000):
 
     env.reset()
     last_action = np.zeros(env.action_dim)
-
+    env.step(np.array([0, 0, -1, 0, 0, 0, 0]))
+    env.render()
     for t in range(timesteps):
         x = np.random.uniform(-0.011, 0.011)
         y = np.random.uniform(-0.011, 0.011)
@@ -39,40 +43,96 @@ def collect_sampled_trajectory(env, timesteps=1000):
         last_action = action
         
 
-def playback_trajectory(env, ep_dir):
-    """Playback data from an episode.
+def gather_demonstrations_as_hdf5(directory, out_dir, env_info):
+    """
+    Gathers the demonstrations saved in @directory into a
+    single hdf5 file.
+
+    The strucure of the hdf5 file is as follows.
+
+    data (group)
+        date (attribute) - date of collection
+        time (attribute) - time of collection
+        repository_version (attribute) - repository version used during collection
+        env (attribute) - environment name on which demos were collected
+
+        demo1 (group) - every demonstration has a group
+            model_file (attribute) - model xml string for demonstration
+            states (dataset) - flattened mujoco states
+            actions (dataset) - actions applied during demonstration
+
+        demo2 (group)
+        ...
 
     Args:
-        env (MujocoEnv): environment instance to playback trajectory in
-        ep_dir (str): The path to the directory containing data for an episode.
+        directory (str): Path to the directory containing raw demonstrations.
+        out_dir (str): Path to where to store the hdf5 file.
+        env_info (str): JSON-encoded string containing environment information,
+            including controller and robot info
     """
 
-    # first reload the model from the xml
-    xml_path = os.path.join(ep_dir, "model.xml")
-    with open(xml_path, "r") as f:
-        env.reset_from_xml_string(f.read())
+    hdf5_path = os.path.join(out_dir, "demo.hdf5")
+    f = h5py.File(hdf5_path, "w")
 
-    state_paths = os.path.join(ep_dir, "state_*.npz")
+    # store some metadata in the attributes of one group
+    grp = f.create_group("data")
 
-    # read states back, load them one by one, and render
-    t = 0
-    for state_file in sorted(glob(state_paths)):
-        print(state_file)
-        dic = np.load(state_file)
-        states = dic["states"]
-        for state in states:
-            env.sim.set_state_from_flattened(state)
-            env.sim.forward()
-            env.render()
-            t += 1
-            if t % 100 == 0:
-                print(t)
+    num_eps = 0
+    env_name = None  # will get populated at some point
+
+    for ep_directory in os.listdir(directory):
+
+        state_paths = os.path.join(directory, ep_directory, "state_*.npz")
+        states = []
+        actions = []
+
+        for state_file in sorted(glob(state_paths)):
+            dic = np.load(state_file, allow_pickle=True)
+            env_name = str(dic["env"])
+
+            states.extend(dic["states"])
+            for ai in dic["action_infos"]:
+                actions.append(ai["actions"])
+
+        if len(states) == 0:
+            continue
+
+        # Add only the successful demonstration to dataset
+        print("Demonstration is being saved")
+            # Delete the last state. This is because when the DataCollector wrapper
+            # recorded the states and actions, the states were recorded AFTER playing that action,
+            # so we end up with an extra state at the end.
+        del states[-1]
+        assert len(states) == len(actions), f"OH NO! {len(states)} != {len(actions)}"
+
+        num_eps += 1
+        ep_data_grp = grp.create_group("demo_{}".format(num_eps))
+
+        # store model xml as an attribute
+        xml_path = os.path.join(directory, ep_directory, "model.xml")
+        with open(xml_path, "r") as f:
+            xml_str = f.read()
+        ep_data_grp.attrs["model_file"] = xml_str
+
+        # write datasets for states and actions
+        ep_data_grp.create_dataset("states", data=np.array(states))
+        ep_data_grp.create_dataset("actions", data=np.array(actions))
+
+    # write dataset attributes (metadata)
+    now = datetime.datetime.now()
+    grp.attrs["date"] = "{}-{}-{}".format(now.month, now.day, now.year)
+    grp.attrs["time"] = "{}:{}:{}".format(now.hour, now.minute, now.second)
+    grp.attrs["repository_version"] = suite.__version__
+    grp.attrs["env"] = env_name
+    grp.attrs["env_info"] = env_info
+
+    f.close()
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--environment", type=str, default="Door")
+    parser.add_argument("--environment", type=str, default="Lift")
     parser.add_argument("--robots", nargs="+", type=str, default="Panda", help="Which robot(s) to use in the env")
     parser.add_argument("--directory", type=str, default="/tmp/")
     parser.add_argument("--timesteps", type=int, default=1000)
